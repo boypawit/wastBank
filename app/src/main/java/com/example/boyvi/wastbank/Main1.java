24a9705c7a5629f9afa6cf35852b510d7b5b4923
boypawit <boyvinai@windowslive.com>
2017-06-23 09:54:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/boyvi/wastbank/Main1.java
@@ -0,0 +1,120 @@
+package com.example.boyvi.wastbank;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class Main1 extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Main1.this);
+        builder.setTitle("                      รู้หรือไม่?");
+        builder.setMessage("ขวด 1 ขวด ลดคาร์บอนไดออกไซด์ได้ 10 กรัม" +
+                             "แก้ว 1 ใบ  ลดคาร์บอนไดออกไซด์ได้ 10 กรัม");   
+        builder.show();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_Main) {
+            // ยังไม่ได้ส่งอะไรไป
+            Intent intent = new Intent(Main1.this,Main1.class);
+            startActivity(intent);
+            // Handle the camera action
+        }else if (id == R.id.nav_camera){
+            Intent intent = new Intent(Main1.this,Main2.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(Main1.this,Main3.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_share) {
+            Intent intent = new Intent(Main1.this,MainActivity.class);
+            startActivity(intent);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/boyvi/wastbank/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.boyvi.wastbank;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText UserOrAdmin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         UserOrAdmin = (EditText)this.findViewById(R.id.editText);
+
+
+
+        Button buttonCountry = (Button) findViewById(R.id.button);
+        buttonCountry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ยังไม่ได้ส่งอะไรไป
+                //User = UserOrAdmin.getText().toString();
+               if(UserOrAdmin.getText().length()==4){
+               // if(User==" user "){
+                Intent intent = new Intent(MainActivity.this,Main1.class);
+                startActivity(intent);
+               }else if (UserOrAdmin.getText().length()==1){
+                   String fake = "Invalid Username or Password";
+                   Toast.makeText(MainActivity.this,fake,Toast.LENGTH_LONG).show();
+
+               }
+
+               else{ Intent intent = new Intent(MainActivity.this,MainAdmin.class);
+                startActivity(intent);}
+
+            }
+        });
+    }
+}
